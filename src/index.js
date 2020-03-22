4ae977d056b27b5eb507787521a5b4cdd40e84d0
Arrow <light.shaft.1@gmail.com>
2020-03-22 18:01:02 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+   
+  if (!array || array.length==0 ) {return 0;}
+  var minValue = Math.min.apply(null, array);
+  return minValue;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length==0 ) {return 0;}
+  var maxValue = Math.max.apply(null, array);
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length==0 ) {return 0;}  var s = 0;
+  var l = array.length;
+   for (i=0; i < l; i++) {
+    s = s +array[i]; 
+   }
+   var avgValue = s / l;
+  return avgValue;
 }
